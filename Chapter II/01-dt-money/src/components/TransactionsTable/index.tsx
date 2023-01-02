148dbe066b2813cd7a0ac1cb0347cc0724f02c47
YagoVela <yagovela@gmail.com>
2023-01-02 18:22:29 -0300

    Componentização

---
+++ Chapter II/01-dt-money/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container> 
+      <table>
+        <thead>
+          <th>Título</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              Desenvolvimento de Web Site
+            </td>
+            <td className="deposit">R$ 121.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>
+              Desenvolvimento de Web Site
+            </td>
+            <td className="withdraw">R$ -121.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
