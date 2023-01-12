cf212f868b6e969c0454d3b810c7dd8015e93a3b
YagoVela <yagovela@gmail.com>
2023-01-12 15:41:57 -0300

    Trabalhando com CMS

--- Chapter III/ignews/src/components/Header/index.tsx
+++ Chapter III/ignews/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
-import { SingInButton } from '../SignInButton';
-import styles from './styles.module.scss';
+import { ActiveLink } from "../ActiveLink";
+import { SingInButton } from "../SignInButton";
+
+import styles from "./styles.module.scss";
 
 export function Header() {
   return (
@@ -7,11 +9,16 @@ export function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Post</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            Home
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts">
+            Posts
+          </ActiveLink>
         </nav>
+
         <SingInButton />
       </div>
     </header>
-  )
+  );
 }
